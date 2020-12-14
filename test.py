919e43916de4a044dcb21dcbd1be3c6fcd11ad92
Logan Wagner <loganwagner56@gmail.com>
2020-12-13 20:45:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,21 @@
 # of importing a class from "source.py", instantiating a new object and printing
 # that object. Replace the code below with your own.
 
-from source import TestClass
 
-t = TestClass(5)
+# Check the Horizontal Alignment of the State Route 18 for example. 
+# The inputs are HorizontalAlignment(x-coordinate, y-coordinate, speed, tangent length).
 
-if __name__ == '__main__':
-    print(t)
+from source import HorizontalAlignment
+
+stateroute18h = HorizontalAlignment(2,5,60,3000)
+
+stateroute18h.solve()
+
+# Now, check the Vertical Alignment of the State Route 18.
+# The inputs are HorizontalAlignment(x-coordinate, y-coordinate, grade of back tangent, grade of forward tangent, speed, type of curve).
+
+from source import VerticalAlignment
+
+stateroute18v = VerticalAlignment(2,5,-0.5,0.2,60,'sag')
+
+stateroute18v.solve()
